bfbacc9c9979de013f7084e9b3ce5cd9546657b3
gyokuhou <57928485+gyokuhou@users.noreply.github.com>
2023-04-21 08:59:11 +0900

    fix:首屏元素不加载

--- force-app/main/default/lwc/order/order.js
+++ force-app/main/default/lwc/order/order.js
@@ -1,8 +1,20 @@
 import { LightningElement, wire, track  } from 'lwc';
-import { subscribe, unsubscribe, MessageContext } from 'lightning/messageService';
+import { subscribe, publish, unsubscribe, MessageContext } from 'lightning/messageService';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import createOrder from '@salesforce/apex/OrderController.createOrder';
 import CHECKOUT_MESSAGE_CHANNEL from '@salesforce/messageChannel/CheckoutMessageChannel__c';
+
+
+import getProducts from '@salesforce/apex/ShoppingCartController.getProducts';
+import deleteProducts from '@salesforce/apex/ShoppingCartController.deleteProducts';
+import updateProductQuantity from '@salesforce/apex/ShoppingCartController.updateProductQuantity';
+
+import SHOPPING_CART_UPDATE_CHANNEL from '@salesforce/messageChannel/ShoppingCartUpdate__c';
+
+const DELAY = 800;
+
+
+
 
 export default class Order extends LightningElement {
     selectedProducts;
@@ -12,6 +24,162 @@ export default class Order extends LightningElement {
     @wire(MessageContext) messageContext;
 
     subscription;
+
+    @track products = [];
+    @track totalPrice = 0;
+    @track selectedProducts = [];
+
+    @wire(MessageContext) messageContext;
+
+    shoppingCartUpdateSubscription;
+    delayTimeout;
+    PictureURL__c;
+    get isCartEmpty() {
+        return this.products.length === 0;
+    }
+
+    connectedCallback() {
+        // Subscribe to ShoppingCartUpdate message
+        this.shoppingCartUpdateSubscription = subscribe(
+            this.messageContext,
+            SHOPPING_CART_UPDATE_CHANNEL,
+            () => this.handleShoppingCartUpdate()
+        );
+    }
+
+    handleShoppingCartUpdate() {
+        // Refresh the products list from the server
+        this.refreshProducts();
+    }
+
+    refreshProducts() {
+        getProducts()
+            .then(data => {
+                this.products = data.map(product => ({ ...product, PictureURL: product.PictureURL__c, isSelected: false }));
+            })
+            .catch(error => {
+                this.showErrorToast('Error Fetching Products', error.body.message);
+            });
+    }
+
+    @wire(getProducts)
+    wiredProducts({ error, data }) {
+        if (data) {
+            this.products = data.map(product => ({
+                ...product, PictureURL: product.PictureURL__c, isSelected: false,rowClass: 'slds-hint-parent'
+            }));
+        } else if (error) {
+            this.showErrorToast('Error Fetching Products', error.body.message);
+        }
+    }
+
+    handleSelectAll(event) {
+        const isSelected = event.target.checked;
+        this.products = this.products.map(product => {
+            return {
+                ...product,
+                isSelected: isSelected,
+                rowClass: isSelected ? 'slds-hint-parent selected-row' : 'slds-hint-parent',
+            };
+        });
+        if (this.selectAllCheckbox.indeterminate) {
+            this.selectAllCheckbox.indeterminate = false;
+        }
+        this.selectedProducts = this.products.filter(product => product.isSelected);
+        this.totalPrice = this.selectedProducts.reduce((total, product) => total + (product.MSRP__c * product.Quantity__c), 0);
+        this.totalPrice = parseFloat(this.totalPrice.toFixed(2));
+    }
+
+    updateSelectAllCheckboxState() {
+        const totalProducts = this.products.length;
+        const selectedProducts = this.products.filter(product => product.isSelected).length;
+
+        this.selectAllCheckbox = this.template.querySelector('input[type="checkbox"]');
+
+        if (selectedProducts === 0) {
+            this.selectAllCheckbox.indeterminate = false;
+            this.selectAllCheckbox.checked = false;
+        } else if (selectedProducts < totalProducts) {
+            this.selectAllCheckbox.indeterminate = true;
+            this.selectAllCheckbox.checked = false;
+        } else {
+            this.selectAllCheckbox.indeterminate = false;
+            this.selectAllCheckbox.checked = true;
+        }
+    }
+
+    handleSelection(event) {
+        const selectedId = event.target.name;
+        this.products.forEach(product => {
+            if (product.Id === selectedId) {
+                product.isSelected = !product.isSelected;
+                product.rowClass = product.isSelected ? 'slds-hint-parent selected-row' : 'slds-hint-parent';
+            }
+        });
+        this.updateSelectAllCheckboxState();
+        this.selectedProducts = this.products.filter(product => product.isSelected);
+        this.totalPrice = this.selectedProducts.reduce((total, product) => total + (product.MSRP__c * product.Quantity__c), 0);
+        this.totalPrice = parseFloat(this.totalPrice.toFixed(2));
+    }
+
+    handleDeleteSelected() {
+        const idsToDelete = this.selectedProducts.map(product => product.Id);
+        deleteProducts({ productIds: idsToDelete })
+            .then(() => {
+                this.products = this.products.filter(product => !idsToDelete.includes(product.Id));
+                this.selectedProducts = [];
+                this.totalPrice = 0;
+                this.showSuccessToast('Success', 'Selected products have been deleted.');
+            })
+            .catch(error => {
+                this.showErrorToast('Error Deleting Products', error.body.message);
+            });
+    }
+
+    handleQuantityChange(event) {
+        const updatedItem = this.products.find(product => product.Id === event.target.name);
+        updatedItem.Quantity__c = event.target.value;
+        this.totalPrice = this.selectedProducts.reduce((total, product) => total + (product.MSRP__c * product.Quantity__c), 0);
+        this.totalPrice = parseFloat(this.totalPrice.toFixed(2));
+        this.delayedFireFilterChangeEvent(updatedItem);
+    }
+
+    handleCheckout() {
+        publish(this.messageContext, CHECKOUT_MESSAGE_CHANNEL, {
+            selectedProducts: this.selectedProducts
+        });
+    }
+
+    delayedFireFilterChangeEvent(updatedItem) {
+        window.clearTimeout(this.delayTimeout);
+        this.delayTimeout = setTimeout(() => {
+            updateProductQuantity({ product: updatedItem })
+                .then(() => {
+                    this.showSuccessToast('Success', 'Quantity updated successfully.');
+                })
+                .catch(error => {
+                    this.showErrorToast('Error Updating Quantity', error.body.message);
+                });
+        }, DELAY);
+    }
+
+    showErrorToast(title, message) {
+        const toastEvent = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: 'error'
+        });
+        this.dispatchEvent(toastEvent);
+    }
+
+    showSuccessToast(title, message) {
+        const toastEvent = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: 'success'
+        });
+        this.dispatchEvent(toastEvent);
+    }
 
     connectedCallback() {
         this.subscribeToMessageChannel();
@@ -87,6 +255,16 @@ export default class Order extends LightningElement {
 
     get isStep3() {
         return this.currentStep === '3';
+    }
+
+    get isNextDisabled() {
+        // Disable the "Next" button if no date is selected in step 2 or it's step 3.
+        return (this.isStep2 && !this.selectedDate) || this.isStep3;
+    }
+
+    connectedCallback() {
+        // Set the initial step to '1' after the component is connected to the DOM.
+        this.currentStep = '1';
     }
 
     handlePrevious() {
